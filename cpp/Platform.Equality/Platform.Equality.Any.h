e1d2e644801238801bcf5339b3623d6730a502b9
uselessgoddess <dodickgod@gmail.com>
2021-04-13 14:56:41 +0300

    Added runtime any equality

--- cpp/Platform.Equality/Platform.Equality.Any.h
+++ cpp/Platform.Equality/Platform.Equality.Any.h
@@ -5,13 +5,55 @@
 
 #include <any>
 #include <functional>
-
-#if __cplusplus >= 201709
+#include <typeindex>
 #include <concepts>
-#endif
+#include <stdexcept>
+
 
 namespace Platform::Equality
 {
+    template<class T>
+    inline auto to_any_hash_visitor(auto func)
+    {
+        return std::pair<std::type_index, std::function<bool(std::any, std::any)>>
+        {
+                std::type_index(typeid(T)),
+                [func](std::any a, std::any b) -> std::size_t
+                {
+                    if constexpr (std::is_void_v<T>)
+                        return func();
+                    else
+                        return func(std::any_cast<T>(a), std::any_cast<T>(b));
+                }
+        };
+    }
+
+    #define BASE_VISITOR_REGISTER(Type) to_any_hash_visitor<Type>([](Type a, Type b) {return a == b;})
+    #define VISITOR_REGISTER(Type, Func) to_any_hash_visitor<Type>([](Type a, Type b) {return Func;})
+    static std::unordered_map<std::type_index, std::function<bool(std::any, std::any)>>
+    any_equal_visitor {
+            BASE_VISITOR_REGISTER(short int),
+            BASE_VISITOR_REGISTER(unsigned short int),
+            BASE_VISITOR_REGISTER(int),
+            BASE_VISITOR_REGISTER(unsigned int),
+            BASE_VISITOR_REGISTER(unsigned long int),
+            BASE_VISITOR_REGISTER(long long int),
+            BASE_VISITOR_REGISTER(unsigned long long int),
+
+            VISITOR_REGISTER(const char*, std::string(a) == std::string(b)),
+
+            BASE_VISITOR_REGISTER(const std::string&),
+
+    };
+    #undef BASE_VISITOR_REGISTER
+    #undef VISITOR_REGISTER
+
+    template<class T>
+    inline void register_any_hash_visitor(auto func)
+    {
+        any_equal_visitor.insert(to_any_hash_visitor<T>(func));
+    }
+
     template<typename TOther>
 #ifdef __cpp_lib_concepts
     requires
@@ -29,6 +71,18 @@ namespace Platform::Equality
     {
         return std::any_cast<TOther>(object) == other;
     }
+
+    bool operator==(const std::any& object, const std::any& other)
+    {
+        if(object.type() != other.type())
+            return false;
+
+        if(!any_equal_visitor.contains(object.type()))
+            throw std::runtime_error(std::string("Equal function for type ").append(object.type().name()).append(" is unregistered"));
+
+        auto equaler = any_equal_visitor[object.type()];
+        return equaler(object, other);
+    }
 }
 
 namespace std
@@ -36,8 +90,7 @@ namespace std
     template<>
     struct equal_to<std::any>
     {
-        template<typename TOther>
-        bool operator()(const std::any& object, TOther other)
+        bool operator()(const std::any& object, const auto& other)
         {
             return Platform::Equality::operator==(object, other);
         }

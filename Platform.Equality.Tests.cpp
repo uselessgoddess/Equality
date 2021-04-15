6d6cfd5a03e1f7035c36f42602099a7d00c92c6f
uselessgoddess <68294279+uselessgoddess@users.noreply.github.com>
2021-04-15 22:53:44 +0300

    Added Google Tests

---
+++ Platform.Equality.Tests.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+#include <Platform.Equality.h>
+
+using namespace Platform::Equality;
+
+namespace PlatformEqualityTests
+{
+    TEST(EqualityTest, BaseAnyEqual)
+    {
+        {
+            int a = 0x228;
+            int b = 0x1337;
+
+            ASSERT_TRUE(std::any(a) != std::any(b));
+        }
+
+        {
+            int a = 0x177013;
+            float b = 0x177013;
+
+            ASSERT_TRUE(std::any(a) != std::any(b));
+        }
+
+        {
+            auto a = "i love green stuff";
+            auto b = (std::string)"i love green stuff";
+
+            ASSERT_TRUE(std::any(a) != std::any(b));
+        }
+
+        {
+            std::vector<int> a {1, 2, 3, 4};
+            std::vector<int> b {1, 2, 3, 4};
+
+            EXPECT_ANY_THROW(std::any(a) == std::any(b));
+        }
+    }
+
+    TEST(EqualityTest, AggressiveAnyTest)
+    {
+        {
+            int steps = 1e6;
+            while(steps--)
+            {
+                int number = rand();
+                ASSERT_TRUE(std::any(number) == std::any(number));
+            }
+        }
+
+        {
+            int steps = 1e6;
+            while(steps--)
+            {
+                int a = rand();
+                int b = rand();
+
+                if(a == b)
+                {
+                    ASSERT_TRUE(std::any(a) == std::any(b));
+                }
+                else
+                {
+                    ASSERT_TRUE(std::any(a) != std::any(b));
+                }
+            }
+        }
+    }
+}
---
+++ Platform.Equality.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef PLATFORM_EQUALITY_H
+#define PLATFORM_EQUALITY_H
+
+#include "Platform.Equality.Any.h"
+
+#endif //PLATFORM_EQUALITY_H

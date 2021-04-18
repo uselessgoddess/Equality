7395b89f6c59b6304c56807df286f6f0a898c308
uselessgoddess <dodickgod@gmail.com>
2021-04-18 10:32:06 +0300

    Small fixes

--- cpp/Platform.Equality/Platform.Equality.Any.h
+++ cpp/Platform.Equality/Platform.Equality.Any.h
@@ -18,18 +18,18 @@ namespace Platform::Equality
         {
             return std::pair<std::type_index, std::function<bool(const std::any&, const std::any&)>>
             {
-                    std::type_index(typeid(TValue)),
-                    [func = std::forward<TEqualityComparer>(func)](const std::any& left, const std::any& right) -> std::size_t
+                std::type_index(typeid(TValue)),
+                [func = std::forward<TEqualityComparer>(func)](const std::any& left, const std::any& right) -> std::size_t
+                {
+                    if constexpr (std::is_void_v<TValue>)
                     {
-                        if constexpr (std::is_void_v<TValue>)
-                        {
-                            return func();
-                        }
-                        else
-                        {
-                            return func(std::any_cast<TValue>(left), std::any_cast<TValue>(right));
-                        }
+                        return func();
                     }
+                    else
+                    {
+                        return func(std::any_cast<TValue>(left), std::any_cast<TValue>(right));
+                    }
+                }
             };
         }
 
@@ -37,18 +37,18 @@ namespace Platform::Equality
         static std::unordered_map<std::type_index, std::function<bool(const std::any&, const std::any&)>>
         AnyEqualityComparers
         {
-                BASE_VISITOR_REGISTER(short int),
-                BASE_VISITOR_REGISTER(unsigned short int),
-                BASE_VISITOR_REGISTER(int),
-                BASE_VISITOR_REGISTER(unsigned int),
-                BASE_VISITOR_REGISTER(unsigned long int),
-                BASE_VISITOR_REGISTER(long long int),
-                BASE_VISITOR_REGISTER(unsigned long long int),
-                BASE_VISITOR_REGISTER(float),
-                BASE_VISITOR_REGISTER(double),
-                BASE_VISITOR_REGISTER(long double),
-                BASE_VISITOR_REGISTER(const char*),
-                BASE_VISITOR_REGISTER(const std::string&),
+            BASE_VISITOR_REGISTER(short int),
+            BASE_VISITOR_REGISTER(unsigned short int),
+            BASE_VISITOR_REGISTER(int),
+            BASE_VISITOR_REGISTER(unsigned int),
+            BASE_VISITOR_REGISTER(unsigned long int),
+            BASE_VISITOR_REGISTER(long long int),
+            BASE_VISITOR_REGISTER(unsigned long long int),
+            BASE_VISITOR_REGISTER(float),
+            BASE_VISITOR_REGISTER(double),
+            BASE_VISITOR_REGISTER(long double),
+            BASE_VISITOR_REGISTER(const char*),
+            BASE_VISITOR_REGISTER(const std::string&),
         };
         #undef BASE_VISITOR_REGISTER
     }
@@ -56,22 +56,11 @@ namespace Platform::Equality
     template<class T>
     inline void RegisterEqualityComparer(auto&& func)
     {
-        Internal::AnyEqualityComparers.insert(Internal::ToAnyEqualVisitor<T>(func));
+        Internal::AnyEqualityComparers.insert(Internal::ToAnyEqualVisitor<T>(std::forward<decltype(func)>(func)));
     }
 
     template<typename TOther>
-#ifdef __cpp_lib_concepts
-    requires
-        requires(TOther left, TOther right)
-        {
-            {std::equal_to<TOther>{}(left, right)} -> std::same_as<bool>;
-        }
-        ||
-        requires(TOther left, TOther right)
-        {
-            {left == right} -> std::same_as<bool>;
-        }
-#endif
+    requires std::equality_comparable<TOther>
     bool operator==(const std::any& object, TOther other)
     {
         return std::any_cast<TOther>(object) == other;
@@ -91,7 +80,7 @@ namespace Platform::Equality
                                                 .append(" is unregistered"));
         }
 
-        auto comparer = Internal::AnyEqualityComparers[object.type()];
+        const auto& comparer = Internal::AnyEqualityComparers[object.type()];
         return comparer(object, other);
     }
 }
@@ -101,7 +90,7 @@ namespace std
     template<>
     struct equal_to<std::any>
     {
-        bool operator()(const std::any& object, const auto& other) const
+        constexpr bool operator()(const std::any& object, const auto& other) const
         {
             return Platform::Equality::operator==(object, other);
         }

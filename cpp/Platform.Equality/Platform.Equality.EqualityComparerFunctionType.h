10d2833e56e704dd623df2e5380ccd774e7f1f44
uselessgoddess <dodickgod@gmail.com>
2021-04-29 15:59:45 +0300

    Small fixes

--- cpp/Platform.Equality/Platform.Equality.EqualityComparerFunctionType.h
+++ cpp/Platform.Equality/Platform.Equality.EqualityComparerFunctionType.h
@@ -1,5 +1,5 @@
-#ifndef PLATFORM_EQUALITY_BASE
-#define PLATFORM_EQUALITY_BASE
+#ifndef PLATFORM_EQUALITY_EQUALITYCOMPARERFUNCTIONTYPE
+#define PLATFORM_EQUALITY_EQUALITYCOMPARERFUNCTIONTYPE
 
 #include <functional>
 
@@ -9,4 +9,4 @@ namespace Platform::Equality
     using EqualityComparerFunctionType = std::function<bool(const TValue&, const TValue&)>;
 }
 
-#endif//PLATFORM_EQUALITY_BASE
+#endif//PLATFORM_EQUALITY_EQUALITYCOMPARERFUNCTIONTYPE
